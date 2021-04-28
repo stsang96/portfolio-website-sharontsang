e49ede550553941e2331c3c26c463bd92556c056
Sharon Tsang <sharontsang96@yahoo.com>
2021-04-27 22:42:08 -0700

    Added Hamburger Menu
    -added an menu icon within HTML coding underneath the <header> below contact link
    -added the CSS stylings for hamburger menu placement
    -included the javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 console.log("Hi, it's nice to meet you!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+  }
+}

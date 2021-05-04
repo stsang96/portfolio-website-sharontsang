1f334c3e2c144504084a69ee1112bab2aea45fa8
Sharon Tsang <sharontsang96@yahoo.com>
2021-05-03 23:17:37 -0700

    - link consistency with HTML and JS
    -deleted responsive from HTML
    -added the fontawesome link to the other HTML pages
    -fleshed out the JS code from lesson

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-console.log("Hi, it's nice to meet you!");
-
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  x.className += ' responsive';
-} else {
-  x.className = 'navtoggle';
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
   }
 }
